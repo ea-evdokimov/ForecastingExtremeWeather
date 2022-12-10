98b3feda684529e43d03d9e48ad546248a69b336
yabatrakov <yabatr>
2022-12-10 14:26:11 +0300

    Target regexp

--- lib/target_markup.py
+++ lib/target_markup.py
@@ -1,5 +1,6 @@
 import json
 import typing as tp
+import re
 
 import pandas as pd
 from pydantic import BaseModel
@@ -16,41 +17,54 @@ class TAGS(BaseModel):
     GOLOLED: str = 'гололедно-изморозевое отложение'
 
 
+TAGS_INSTANCE = TAGS()
 if __name__ == '__main__':
-    TAGS_INSTANCE = TAGS()
-    with open('tags.json', 'w') as f:
+    with open('TAGS_INSTANCE.json', 'w') as f:
         json.dump(TAGS_INSTANCE.dict(), f, ensure_ascii=False)
 
 
 class Target(BaseModel):
-    VETER: bool = False
-    SHKVAL: bool = False
-    METEL: bool = False
-    DOZD: bool = False
-    SNEG: bool = False
-    GRAD: bool = False
-    TUMAN: bool = False
-    GOLOLED: bool = False
+    values = {
+        TAGS_INSTANCE.VETER: False,
+        TAGS_INSTANCE.SHKVAL: False,
+        TAGS_INSTANCE.METEL: False,
+        TAGS_INSTANCE.DOZD: False,
+        TAGS_INSTANCE.SNEG: False,
+        TAGS_INSTANCE.GRAD: False,
+        TAGS_INSTANCE.TUMAN: False,
+        TAGS_INSTANCE.GOLOLED: False,
+    }
 
 
 def classify(row: pd.Series) -> tp.Dict:
     target = Target()
 
-    # maybe check W1 and W2 not only WW
-    if row['Ff'] >= 20.0 or row['ff10'] >= 25.0:  # ff10 and ff3
-        target.VETER = True
+    WW, W1, W2 = str(row['WW']), str(row['W1']), str(row['W1'])
+    regexps = {
+        TAGS_INSTANCE.VETER: re.compile(r'ветер|ветр', re.I),
+        TAGS_INSTANCE.SHKVAL: re.compile(r'шквал', re.I),
+        TAGS_INSTANCE.METEL: re.compile(r'метел', re.I),
+        TAGS_INSTANCE.DOZD: re.compile(r'дожд|ливен|морос', re.I),
+        TAGS_INSTANCE.SNEG: re.compile(r'снег|снежн|круп|зерн', re.I),
+        TAGS_INSTANCE.GRAD: re.compile(r'град', re.I),
+        TAGS_INSTANCE.TUMAN: re.compile(r'туман|мгла', re.I),
+        TAGS_INSTANCE.GOLOLED: re.compile(r'голол[её]д', re.I),
+    }
+    for tag, regexp in regexps.items():
+        if regexp.search(WW) or regexp.search(W1) or regexp.search(W2):
+            target.values[tag] = True
 
-    if 'шквал' in row['WW'].lower():
-        target.SHKVAL = True
+    if row['Ff'] >= 20.0 or row['ff10'] >= 25.0:  # ff10 and ff3
+        target.values[TAGS_INSTANCE.VETER] = True
 
     if all([
-        'снег' in row["E'"].lower(),
+        'снег' in str(row["E'"]).lower(),
         row['Ff'] >= 15.0,
-        row['VV'] <= 0.5,
-    ]) or 'метель' in row['WW'].lower():
-        target.METEL = True
+        # row['VV'] <= 0.5,
+    ]):
+        target.values[TAGS_INSTANCE.METEL] = True
 
-    if row['RRR'] >= 50.0 and row['tR'] <= 12:  # check WW
-        target.DOZD = True
+    # if row['RRR'] >= 50.0 and row['tR'] <= 12:
+    #     target.values[TAGS_INSTANCE.DOZD] = True
 
     return target.dict()
